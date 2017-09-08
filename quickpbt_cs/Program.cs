69927671794a98c703357ff915ecb6ad2de25d87
Paul Blasucci <P.Blasucci@evision-software.com>
2017-09-08 11:16:45 +0200

    basic setup with smoke tests for all 3 languages

---
+++ quickpbt_cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace quickpbt
+{
+  public class Scratch
+  {
+    [Fact]
+    public void UnitTest()
+    {
+      Assert.True(1 == 1);
+    }
+
+    [Property]
+    public bool PropertyTest(int x)
+    {
+      return (x == x);
+    }
+  }
+}
---
+++ quickpbt_fs/Program.fs
@@ -0,0 +1,15 @@
+﻿namespace quickpbt
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+
+type Scratch() =
+
+  [<Fact>]
+  member __.UnitTest() =
+    Assert.True(1 = 1)
+
+  [<Property>]
+  member __.PropertyTest(x:int) =
+    (x = x)
